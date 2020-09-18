50054d78035c0a870441fe70ef7fa9c759952a9b
SixDevelop <47978391+SixDevelop@users.noreply.github.com>
2020-09-18 19:59:04 +0300

    issue2/add routing and create header component

---
+++ web-client/src/components/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header(){
+    return(
+        <div className="container">
+            <p>hello world</p>
+        </div>
+    )
+}
+
+export default Header
--- web-client/src/index.tsx
+++ web-client/src/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Router } from "react-router-dom"
+import { createBrowserHistory } from "history"
+
 import './index.css';
-import App from './App';
+import Header from './components/header';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const history = createBrowserHistory()
+
+ReactDOM.render((
+  <Router history={history}>
+    <Header/>
+  </Router>
+), document.getElementById('root')
+  
 );
 
 // If you want your app to work offline and load faster, you can change
