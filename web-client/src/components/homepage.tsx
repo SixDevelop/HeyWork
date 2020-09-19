074c1e87cda24fd8b772022b37862bd13311820a
SixDevelop <47978391+SixDevelop@users.noreply.github.com>
2020-09-19 17:50:46 +0300

    issue2/add simple homepage design

--- web-client/src/components/homepage.tsx
+++ web-client/src/components/homepage.tsx
@@ -11,8 +11,15 @@ export default class Homepage extends React.Component{
                 <div className="header">
                     <Header/>
                 </div>
-                
-                <p>it is homepage, bitch!</p>               
+                <div className="homePage">
+                    <div className="textPlate">
+                        <p className="bold">The best way to get started <br/> is to stop talking <br/> and start doing.</p>
+                        <p>HeyWork will help you to find people for your projects</p>
+                    </div>
+                    <div className="imgContainer">
+                        <img src="https://www.blacode.fr/wp-content/uploads/2018/04/hub-business-center.jpg" alt="" className="img"/>
+                    </div>
+                </div>             
             </div>
             </>
         )
